0d2d8320bfa5b77d4cf137fd5483f7078bada6a3
Axel <axel-bertrand@hotmail.fr>
2023-06-16 02:19:16 +0200

    drop/keep roll

--- src/commands/general/roll.rs
+++ src/commands/general/roll.rs
@@ -1,4 +1,5 @@
-use crate::{InteractionMessage, InteractionResponse};
+use crate::{InteractionMessage, InteractionResponse, utils};
+use itertools::Itertools;
 use rand::Rng;
 use serenity::builder::CreateApplicationCommand;
 use serenity::framework::standard::macros::command;
@@ -10,6 +11,62 @@ use serenity::model::prelude::interaction::application_command::{
 use serenity::model::prelude::{ChannelId, Message};
 use serenity::prelude::Context;
 use std::cmp::Ordering;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum DropKeep {
+    D,
+    K,
+    None,
+}
+
+impl FromStr for DropKeep {
+    type Err = Box::<dyn std::error::Error + Send + Sync>;
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> { 
+        match s {
+            "d" => Ok(DropKeep::D),
+            "k" => Ok(DropKeep::K),
+            _ => Ok(DropKeep::None)
+        }
+    }
+}
+
+impl std::fmt::Display for DropKeep {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> { 
+        let s = match self {
+            DropKeep::D => "d",
+            DropKeep::K => "k",
+            DropKeep::None => ""
+        };
+        write!(f, "{s}")
+    }
+}
+
+impl DropKeep {
+    pub fn is_some(self) -> bool {
+        self != DropKeep::None
+    }
+}
+
+#[derive(Debug)]
+pub struct Roll {
+    number: i64,
+    size: i64,
+    modifier: i64,
+    dk: DropKeep,
+    dk_val: Option<i64>,
+}
+
+impl Roll {
+    pub fn builder(number: i64, size: i64, modifier: i64, dk: DropKeep, dk_val: Option<i64>) -> Roll {
+        Roll { number, size, modifier, dk, dk_val }
+    }
+
+    /// (number, size, modifier, dk, dk_val)
+    pub fn values(&self) -> (i64, i64, i64, DropKeep, Option<i64>) {
+        (self.number, self.size, self.modifier, self.dk, self.dk_val)
+    }
+}
 
 #[command]
 #[aliases("r")]
@@ -19,7 +76,7 @@ use std::cmp::Ordering;
 #[example = "3d4-1"]
 #[example = "d8"]
 pub async fn roll(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    roll_intern(ctx, msg.channel_id, args).await?;
+    let _ = roll_intern(ctx, msg.channel_id, args).await?;
     Ok(())
 }
 
@@ -28,63 +85,137 @@ pub async fn roll_intern(
     channel_id: ChannelId,
     args: Args,
 ) -> Result<Message, CommandError> {
-    let d_split: Vec<&str> = args.message().split('d').collect();
-    let size;
-    let n;
-    let mut modifier = 0;
-    if d_split.len() == 2 {
-        n = d_split[0].parse::<i64>().unwrap_or(1);
-        if !(1..=200).contains(&n) {
-            return Err("1 <= nombres de dés <= 200".into());
-        }
-        let (char, pos) = if !d_split[1].contains('-') {
-            ('+', true)
-        } else {
-            ('-', false)
-        };
-        let mod_split: Vec<&str> = d_split[1].split(char).collect();
-        size = mod_split[0].parse::<i64>()?;
-        if size < 1 {
-            return Err("taille du dé >= 1".into());
-        }
-        if mod_split.len() == 2 {
-            modifier = if pos {
-                mod_split[1].parse::<i64>()?
-            } else {
-                -mod_split[1].parse::<i64>()?
-            };
-        }
-    } else {
-        return Err("mauvaise syntaxe, doit être : 'roll <x>d<n>+<y>'".into());
-    }
-    let msg = channel_id.say(&ctx.http, run(size, n, modifier)).await?;
+    let roll = regex_roll(args.message())?;
+    let msg = channel_id.say(&ctx.http, run(roll)).await?;
+
     Ok(msg)
 }
 
-pub fn run(size: i64, n: i64, modifier: i64) -> String {
+#[allow(clippy::invalid_regex)]
+fn regex_roll(roll: impl ToString) -> Result<Roll, CommandError> {
+    let re = match regex::Regex::new(r"(?<number>\d+)?d(?<size>\d+)(?<modifier>\+\d+|-\d+)?(?<dk>k|d)?(?<dk_val>\d+)?") {
+        Ok(r) => r,
+        Err(e) => {
+            return Err(utils::command_error(format!("erreur regex : {e}")));
+        }
+    };
+    let s = &roll.to_string();
+    let caps = match re.captures(s) {
+        Some(caps) => caps,
+        None => return Err(utils::command_error("erreur captures regex")),
+    };
+
+    let number = match caps.name("number") {
+        Some(n) => n.as_str(),
+        None => "1",
+    }.parse::<i64>()?;
+    if !(1..=100).contains(&number) {
+        return Err(utils::command_error("le nombre de dés doit appartenir à [1; 200]"));
+    }
+    let size = match caps.name("size") {
+        Some(m) => m.as_str().parse::<i64>()?,
+        None => return Err(utils::command_error("erreur pas de taille de dé"))
+    };
+    if size <= 1 {
+        return Err(utils::command_error("la taille du dé doit être supérieure strictement à 1"));
+    }
+    let modifier = match caps.name("modifier") {
+        Some(n) => n.as_str(),
+        None => "0",
+    }.parse::<i64>()?;
+    let dk: DropKeep = DropKeep::from_str(match caps.name("dk") {
+        Some(m) => m.as_str(),
+        None => "",
+    })?;
+    let dk_val = match caps.name("dk_val") {
+        Some(m) => Some(m.as_str().parse::<i64>()?),
+        None => None
+    };
+    if dk.is_some() && dk_val.is_none() {
+        return Err(utils::command_error("valeur attendu après le drop/keep"));
+    }
+    let roll = Roll::builder(number, size, modifier, dk, dk_val);
+    Ok(roll)
+}
+
+pub fn run(roll: Roll) -> String {
+    let (n, size, modifier, dk, dk_val) = roll.values();
+    let start = start_message(&roll);
+    let mut rolls: Vec<i64> = Vec::new();
     let mut rng = rand::thread_rng();
-    let mut res: String = String::new();
-    let mut sum = modifier;
 
     for _ in 0..n {
-        let roll = rng.gen_range(1..=size);
-        sum += roll;
-        res = format!("{}{}{}", res, roll, " + ");
+        rolls.push(rng.gen_range(1..=size));
     }
-    res = res[..res.len() - 2].to_string(); //remove last "+ "
 
-    let (mut res, show_mod) = match modifier.cmp(&0) {
-        Ordering::Greater => (format!("{}(+{}) ", res, modifier), format!("+{modifier}")),
-        Ordering::Less => (format!("{}({}) ", res, modifier), modifier.to_string()),
-        Ordering::Equal => (res, String::new()),
+    let initial_roll = rolls_str(&rolls, modifier);
+
+    match dk {
+        DropKeep::D => {
+            let drop_rolls = drop_low(rolls, dk_val.unwrap().try_into().unwrap());
+            let new_rolls = format!("({initial_roll}) -> {}", rolls_str(&drop_rolls, modifier));
+            let res = sum(drop_rolls, modifier);
+            final_str(start, res, new_rolls, n, modifier)
+        },
+        DropKeep::K => {
+            let keep_rolls = drop_low(rolls, (n - dk_val.unwrap()).try_into().unwrap());
+            let new_rolls = format!("({initial_roll}) -> {}", rolls_str(&keep_rolls, modifier));
+            let res = sum(keep_rolls, modifier);
+            final_str(start, res, new_rolls, n, modifier)
+        },
+        DropKeep::None => {
+            let res = rolls.iter().sum::<i64>() + modifier;
+            final_str(start, res, initial_roll, n, modifier)
+        }
+    }
+}
+
+fn final_str(start: String, res: i64, rolls: String, n: i64, modifier: i64) -> String {
+    if modifier == 0 && n == 1 {
+        format!("{start} {res}")
+    } else {
+        format!("{start} {rolls} = {res}")
+    }
+}
+
+fn sum(rolls: Vec<i64>, modifier: i64) -> i64 {
+    rolls.iter().sum::<i64>() + modifier
+}
+
+fn rolls_str(rolls: &[i64], modifier: i64) -> String {
+    add_modifier(rolls.iter().map(|r| r.to_string()).collect::<Vec<String>>().join(" + "), modifier)
+}
+
+fn drop_low(mut rolls: Vec<i64>, n: usize) -> Vec<i64> {
+    let bad_elements = rolls.clone().into_iter().sorted().rev().skip(rolls.len() - n).collect::<Vec<i64>>();
+    for elem in bad_elements {
+        let index = rolls.clone().into_iter().position(|e| e == elem).unwrap();
+        rolls.remove(index);
+    }
+    rolls
+}
+
+fn start_message(roll: &Roll) -> String {
+    let (n, size, modifier, dk, dk_val) = roll.values();
+    let show_mod = match modifier.cmp(&0) {
+        Ordering::Greater =>  format!("+{modifier}"),
+        Ordering::Less => modifier.to_string(),
+        Ordering::Equal => String::new(),
     };
-    let dice_number = if n > 1 { n.to_string() } else { String::new() };
-    let start_message = format!("`[{dice_number}d{size}{show_mod}]`");
+    let dk_val = if let Some(x) = dk_val {
+        x.to_string()
+    } else {
+        String::new()
+    };
+    format!("`[{n}d{size}{show_mod}{dk}{dk_val}]`")
+}
 
-    if modifier != 0 || n > 1 {
-        res = format!("{}= {}", res, sum);
+fn add_modifier(s: String, modifier: i64) -> String {
+    match modifier.cmp(&0) {
+        Ordering::Greater => format!("{s} (+{modifier})"),
+        Ordering::Less => format!("{s} ({modifier})"),
+        Ordering::Equal => s
     }
-    format!("{start_message} {res}")
 }
 
 pub fn run_chat_input(options: &[CommandDataOption]) -> InteractionResponse {
@@ -113,9 +244,10 @@ pub fn run_chat_input(options: &[CommandDataOption]) -> InteractionResponse {
             _ => (),
         }
     }
+    let roll = Roll::builder(n, size, modifier, DropKeep::None, None);
 
     InteractionResponse::Message(InteractionMessage {
-        content: run(size, n, modifier),
+        content: run(roll),
         ephemeral: false,
         embed: None,
     })
@@ -131,7 +263,6 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
                 .description("Taille des dés")
                 .kind(CommandOptionType::Integer)
                 .min_int_value(2)
-                .max_int_value(1000)
                 .required(true)
         })
         .create_option(|option| {
@@ -140,7 +271,7 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
                 .description("Nombre de dés")
                 .kind(CommandOptionType::Integer)
                 .min_int_value(1)
-                .max_int_value(100)
+                .max_int_value(200)
         })
         .create_option(|option| {
             option
